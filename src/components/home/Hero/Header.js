8ae61eacb9af4fd9a19039b1b3080453acbd79ca
Matt <symplymatt@gmail.com>
2022-09-23 12:06:08 +0100

    nav-links

--- src/components/home/Hero/Header.js
+++ src/components/home/Hero/Header.js
@@ -4,6 +4,12 @@ const Header = () => {
     return <div className='header-container'>
         <div className="logo-container"><img src="images/paylode-logo.png" alt="" /></div>
         <div className="line"></div>
+        <div className="nav-links-container">
+            <a href="">Why Paylode</a>
+            <a href="">Pricing</a>
+            <a href="">For Developers</a>
+            <a href="">Support</a>
+        </div>
   </div>;
 };
 
