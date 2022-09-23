5b9a094221498e89a410cfcefb58bc7178def4e7
Matt <symplymatt@gmail.com>
2022-09-23 11:45:49 +0100

    logo

---
+++ src/components/home/Hero/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return <div className='header-container'>
+        <div className="logo-container"><img src="images/paylode-logo.png" alt="" /></div>
+        <div className="line"></div>
+  </div>;
+};
+
+export default Header;
---
+++ src/components/home/Hero/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+const Hero = () => {
+  return (
+    <div className='hero-container'>
+      <Header />
+      <Main />
+    </div>
+  );
+};
+
+export default Hero;
