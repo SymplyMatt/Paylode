701fe7de3d53c9d03d471adfd774778866db3728
Matt <symplymatt@gmail.com>
2022-09-23 17:44:25 +0100

    header overlay

--- src/components/home/Hero/header/NavLinks.js
+++ src/components/home/Hero/header/NavLinks.js
@@ -1,11 +1,19 @@
 import React from "react";
-
+import { useState } from "react";
 const NavLinks = () => {
+  const [overlay, setOverlay] = useState(false);
   return (
     <div className='nav-links-container'>
       <a href=''>Why Paylode</a>
       <a href=''>Pricing</a>
       <a href=''>For Developers</a>
+      <div className="hover" onMouseEnter={()=>setOverlay(true)}>
+        <i class='fa-solid fa-circle-chevron-down'></i>
+        <div className={`overlay ${!overlay && 'hide'}`} onMouseEnter={()=>setOverlay(true)} onMouseLeave={()=>setOverlay(false)}>
+          <a href=""> Overview</a>
+          <a href="">API Documentation</a>
+        </div>
+      </div>
       <a href=''>Support</a>
     </div>
   );
