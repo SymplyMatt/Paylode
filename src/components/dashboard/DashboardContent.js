c086113072b4d4c5c2781dab9d43ebd6dd5457e6
Matt <symplymatt@gmail.com>
2022-09-29 10:32:15 +0100

    dashboard page unresponsive

--- src/components/dashboard/DashboardContent.js
+++ src/components/dashboard/DashboardContent.js
@@ -20,7 +20,56 @@ const DashboardContent = () => {
             <div className='show-overlay flex-row-center'>Last Year</div>
         </div>
         <AnalyticsOverview />
-        <Chart />
+      <Chart />
+      <div className='dashboard-analytics-container flex-column-center gap-20'>
+        <div className='analytics-text'>Transaction History</div>
+        <div className='dashboard-graph'>
+          <div className="dashboard-grid dashboard-grid-heading">
+            <div className="">Transaction Reference</div>
+            <div className="">Amount</div>
+            <div className="">Payment Method</div>
+            <div className="">Status</div>
+          </div>
+          <div className="dashboard-grid dashboard-grid-item">
+            <div className="flex-row gap-10">
+              <div className="dashboard-item-icon"><img src="images/inflow-icon.svg" alt="" /></div>
+              <div className="flex-column">
+                <div className="transaction-reference-text">Payment from <span> #10023</span></div>
+                <div className="transaction-reference-text-sm">Today, 10:30 AM</div>
+              </div>
+            </div>
+            <div className="amount">+ $650.00</div>
+            <div className="amount">Card Payment</div>
+            <div className="status completed">Completed</div>
+          </div>
+          <div className="dashboard-grid dashboard-grid-item">
+            <div className="flex-row gap-10">
+              <div className="dashboard-item-icon"><img src="images/outflow-icon.svg" alt="" /></div>
+              <div className="flex-column">
+                <div className="transaction-reference-text">Payment from <span> #10023</span></div>
+                <div className="transaction-reference-text-sm">Today, 10:30 AM</div>
+              </div>
+            </div>
+            <div className="amount">+ $650.00</div>
+            <div className="amount">Card Payment</div>
+            <div className="status completed">Completed</div>
+          </div>
+          <div className="dashboard-grid dashboard-grid-item">
+            <div className="flex-row gap-10">
+              <div className="dashboard-item-icon"><img src="images/failed-icon.svg" alt="" /></div>
+              <div className="flex-column">
+                <div className="transaction-reference-text">Payment from <span> #10023</span></div>
+                <div className="transaction-reference-text-sm">Today, 10:30 AM</div>
+              </div>
+            </div>
+            <div className="amount">- $650.00</div>
+            <div className="amount">Card Payment</div>
+            <div className="status declined">Declined</div>
+          </div>
+          
+          <div className="view-all">View All Transactions</div>
+        </div>
+      </div>
     </div>
   );
 };
