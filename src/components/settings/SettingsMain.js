85554c7b23c9452225f704eff7ae2fc39b7bc0e4
Matt <symplymatt@gmail.com>
2022-10-01 11:05:34 +0100

    update profile unresponsive

---
+++ src/components/settings/SettingsMain.js
@@ -0,0 +1,124 @@
+import React from "react";
+
+const SettingsMain = ({
+  pushToggle,
+  setPushToggle,
+  settingsPage,
+  setSettingsPage,
+}) => {
+  return (
+    <div className='settings-container'>
+      <div className='flex-column-center gap-10 settings-header'>
+        <div className='settings-title'>Customize Settings</div>
+        <div className='settings-header-desc'>
+          Personalize settings to suit your company
+        </div>
+        <div className=''>
+          <hr />
+        </div>
+      </div>
+
+      <div className='flex-column gap-30 item-container'>
+        <div
+          className='flex-row justify-sb gap-30 pointer'
+          onClick={() => setSettingsPage("update-profile")}
+        >
+          <div className='flex-column gap-10'>
+            <div className='item-title'>Update Profile Information</div>
+            <div className='item-desc'>
+              Update your personal profile informations here
+            </div>
+          </div>
+          <div className=''>
+            <i class='fa-solid fa-arrow-right pointer'></i>
+          </div>
+        </div>
+        <hr />
+      </div>
+      <div className='flex-column gap-30 item-container'>
+        <div className='flex-row justify-sb gap-30 pointer'>
+          <div className='flex-column gap-10'>
+            <div className='item-title'>Bank Account</div>
+            <div className='item-desc'>
+              Update your bank account informations here delete or add new
+              account
+            </div>
+          </div>
+          <div className=''>
+            <i class='fa-solid fa-arrow-right pointer'></i>
+          </div>
+        </div>
+        <hr />
+      </div>
+      <div className='flex-column gap-30 item-container'>
+        <div className='flex-row justify-sb gap-30 pointer'>
+          <div className='flex-column gap-10'>
+            <div className='item-title'>Api Key & Webhooks</div>
+            <div className='item-desc'>
+              Update your bank account informations here delete or add new
+              account
+            </div>
+          </div>
+          <div className=''>
+            <i class='fa-solid fa-arrow-right pointer'></i>
+          </div>
+        </div>
+        <hr />
+      </div>
+      <div className='flex-column gap-30 item-container'>
+        <div className='flex-row justify-sb gap-30 pointer'>
+          <div className='flex-column gap-10'>
+            <div className='item-title'>Preferences</div>
+            <div className='item-desc'>
+              Update your bank account informations here delete or add new
+              account
+            </div>
+          </div>
+          <div className=''>
+            <i class='fa-solid fa-arrow-right pointer'></i>
+          </div>
+        </div>
+        <hr />
+      </div>
+      <div className='flex-column gap-30 item-container'>
+        <div className='flex-row justify-sb gap-30'>
+          <div className='flex-column gap-10'>
+            <div className='item-desc'>Security & Privacy</div>
+            <div className='item-title pointer'>Change Password</div>
+          </div>
+          <div className=''>
+            <i class='fa-solid fa-arrow-right pointer'></i>
+          </div>
+        </div>
+        <hr />
+      </div>
+      <div className='flex-column gap-30 item-container'>
+        <div className='flex-row justify-sb gap-30'>
+          <div className='flex-column gap-10'>
+            <div className='item-title'>Browser Push Notifications</div>
+            <div className='item-desc pointer'>
+              Allow web push notifications
+            </div>
+          </div>
+          <div className=''>
+            {!pushToggle && (
+              <i
+                class='fa-solid fa-toggle-off pointer'
+                onClick={() => setPushToggle(true)}
+              ></i>
+            )}
+            {pushToggle && (
+              <i
+                class='fa-solid fa-toggle-on pointer'
+                onClick={() => setPushToggle(false)}
+              ></i>
+            )}
+          </div>
+        </div>
+        <hr />
+      </div>
+    </div>
+  );
+};
+
+export default SettingsMain;
---
+++ src/components/settings/UpdateProfile.js
@@ -0,0 +1,52 @@
+import React from "react";
+import ReactFlagsSelect from "react-flags-select";
+import { useState } from "react";
+import { useEffect } from "react";
+
+const UpdateProfile = () => {
+    const [selected, setSelected] = useState("NG");
+    return (
+    <div className="settings-container">
+        <div className="back-icon-container">
+          <i class='fa-solid fa-arrow-left pointer'></i><span>Back</span>
+        </div>
+        <div className="">
+            <div className='flex-column-center gap-10 settings-header'>
+                <div className='settings-title'>Customize Settings</div>
+                <div className='settings-header-desc'>Personalize settings to suit your company</div>
+                <div className=''><hr /></div>
+            </div>
+        </div>
+        <div className="flex-column gap-20">
+            <div className="flex-row gap-15 input-flex">
+                <div className='input-one-container update-profile'>
+                    <input type='text' placeholder='First Name' className='input-box input-settings'/>
+                </div>
+                <div className='input-one-container update-profile'>
+                    <input type='text' placeholder='Last Name' className='input-box input-settings'/>
+                </div>
+            </div>
+            <div className='input-one-container update-profile'>
+                <input type='text' placeholder='Email Address' className='input-box input-settings'/>
+            </div>
+            <div className='input-one-container update-profile'>
+                <ReactFlagsSelect
+                    selected={selected}
+                    onSelect={(code) => setSelected(code)}
+                    countries={["NG"]}
+                    selectButtonClassName='menu-flags-button'
+                    showSecondarySelectedLabel={false}
+                    showOptionLabel={false}
+                    fullWidth={false}
+                    showSecondaryOptionLabel={false}
+                    className='menu-flags'
+                />
+                <input type='text' placeholder='Phone Number' className='input-box input-settings'/>
+            </div>
+        </div>
+        <div className='payments-btn align-left'>Save Changes</div>
+    </div>
+    )
+};
+
+export default UpdateProfile;
